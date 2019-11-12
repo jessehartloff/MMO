7370d7dfe593c69a01a16f0883902c0bd305d4d2
jessehartloff <hartloff@buffalo.edu>
2019-11-11 23:36:52 -0500

    Initial commit

---
+++ src/backend/PathFinding.scala
@@ -0,0 +1,16 @@
+package backend
+
+import backend.physics.PhysicsVector
+
+object PathFinding {
+
+  def findPath(start: GridLocation, end: GridLocation, map: List[List[MapTile]]): List[GridLocation] = {
+    null
+  }
+
+
+  def getVelocity(path: List[GridLocation], currentLocation: PhysicsVector): PhysicsVector = {
+    null
+  }
+
+}
